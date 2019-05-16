866ff7d282d80fd6fd9783e3f3ae7251523c2360
Arjen Hiemstra <ahiemstra@heimr.nl>
2019-05-16 19:45:49 +0200

    Implement from-to range for PieChart
    So we can select what to display

--- src/PieChart.h
+++ src/PieChart.h
@@ -15,6 +15,14 @@ class PieChart : public QQuickItem
     Q_OBJECT
 
     /**
+     * The value at which this PieChart will start counting.
+     */
+    Q_PROPERTY(qreal from READ from WRITE setFrom NOTIFY fromChanged)
+    /**
+     * The value at which this PieChart is considered "full". If this is -1, the PieChart will ensure
+     */
+    Q_PROPERTY(qreal to READ to WRITE setTo NOTIFY toChanged)
+    /**
      * The width of the PieChart in border rendering mode. If this is -1, the PieChart will use a completely filled circle.
      */
     Q_PROPERTY(qreal borderWidth READ borderWidth WRITE setBorderWidth NOTIFY borderWidthChanged)
@@ -38,16 +46,22 @@ public:
      */
     ~PieChart();
 
+    qreal from() const;
+    qreal to() const;
     qreal borderWidth() const;
     DataSource* valueSource() const;
     DataSource* colorSource() const;
 
 public Q_SLOTS:
+    void setFrom(qreal from);
+    void setTo(qreal to);
     void setBorderWidth(qreal width);
     void setValueSource(DataSource* value);
     void setColorSource(DataSource* color);
 
 Q_SIGNALS:
+    void fromChanged();
+    void toChanged();
     void borderWidthChanged();
     void valueSourceChanged();
     void colorSourceChanged();
