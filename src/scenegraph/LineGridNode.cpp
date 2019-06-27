9a3f820fea42d22fed68f28b5fd2bc415c5160f2
Arjen Hiemstra <ahiemstra@heimr.nl>
2019-06-27 15:44:49 +0200

    Fix LineGridNode so it behaves correctly
    Now we always display a line for left/right/top/bottom to clearly
    indicate 0

--- src/scenegraph/LineGridNode.cpp
+++ src/scenegraph/LineGridNode.cpp
@@ -35,8 +35,6 @@ void LineGridNode::setVertical(bool vertical)
         return;
 
     m_vertical = vertical;
-//     update();
-//     setFlag(QSGNode::UsePreprocess, true);
 }
 
 void LineGridNode::setRect(const QRectF& rect)
@@ -45,8 +43,6 @@ void LineGridNode::setRect(const QRectF& rect)
         return;
 
     m_rect = rect;
-//     update();
-//     setFlag(QSGNode::UsePreprocess, true);
 }
 
 void LineGridNode::setColor(const QColor& color)
@@ -64,8 +60,6 @@ void LineGridNode::setSpacing(float spacing)
         return;
 
     m_spacing = spacing;
-//     update();
-//     setFlag(QSGNode::UsePreprocess, true);
 }
 
 void LineGridNode::setLineWidth(float lineWidth)
@@ -91,12 +85,10 @@ void LineGridNode::update()
 
     int totalVertices = 0;
     if (!m_vertical) {
-        totalVertices = int(m_rect.width() / m_spacing - 1) * 2;
+        totalVertices = int(m_rect.width() / m_spacing) * 2 + 4;
     } else {
-        totalVertices = int(m_rect.height() / m_spacing) * 2;
+        totalVertices = int(m_rect.height() / m_spacing) * 2 + 4;
     }
-
-    qDebug() << "verts" << totalVertices << m_rect.width() << m_spacing;
 
     if (totalVertices != m_geometry->vertexCount()) {
         m_geometry->allocate(totalVertices, totalVertices);
@@ -107,19 +99,26 @@ void LineGridNode::update()
 
     int index = 0;
     if (m_vertical) {
-        for (auto i = m_rect.height() - m_spacing; i > 0.0; i -= m_spacing) {
+        line(vertices, indices, index, m_rect.left(), m_rect.top(), m_rect.right(), m_rect.top());
+
+        for (auto i = m_spacing; i < m_rect.height(); i += m_spacing) {
             line(vertices, indices, index, m_rect.left(), i, m_rect.right(), i);
         }
+
+        line(vertices, indices, index, m_rect.left(), m_rect.bottom(), m_rect.right(), m_rect.bottom());
     } else {
-        for (auto i = 0.0; i < m_rect.width(); i += m_spacing) {
+        line(vertices, indices, index, m_rect.left(), m_rect.top(), m_rect.left(), m_rect.bottom());
+
+        for (auto i = m_spacing; i < m_rect.width(); i += m_spacing) {
             line(vertices, indices, index, i, m_rect.top(), i, m_rect.bottom());
         }
+
+        line(vertices, indices, index, m_rect.right(), m_rect.top(), m_rect.right(), m_rect.bottom());
     }
 
     m_geometry->markVertexDataDirty();
     m_geometry->markIndexDataDirty();
     markDirty(QSGNode::DirtyGeometry);
-//     setFlag(QSGNode::UsePreprocess, false);
 }
 
 void LineGridNode::line(QSGGeometry::Point2D* vertices, quint16 *indices, int &index, qreal fromX, qreal fromY, qreal toX, qreal toY)
