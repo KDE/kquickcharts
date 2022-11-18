f2d2a05870cb7f64f8370624045e2f97a44c1705
Alexander Lohnau <alexander.lohnau@gmx.de>
2022-11-18 20:24:08 +0100

    Fix some minor reuse licensing issues
    - Add SPDX info to autotests/CMakeLists.txt file
    - Download missing licenses

--- autotests/qmltest.cpp
+++ autotests/qmltest.cpp
@@ -1,6 +1,7 @@
 /*
  * This file is part of KQuickCharts
  * SPDX-FileCopyrightText: 2019 Arjen Hiemstra <ahiemstra@heimr.nl>
+ * SPDX-FileCopyrightText: 2022 Alexander Lohnau <alexander.lohnau@gmx.de>
  *
  * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
  */
