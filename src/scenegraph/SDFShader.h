e1cca9c856edee529360bcd96f70389001add77d
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-07-17 14:10:46 +0200

    Introduce UniformDataStream as improved API for shader uniform data
    Raw memcpy is ugly and hard to follow, especially with the offsets.

--- src/scenegraph/SDFShader.h
+++ src/scenegraph/SDFShader.h
@@ -10,6 +10,83 @@
 
 #include <QSGMaterialShader>
 
+struct UniformDataStream {
+    inline UniformDataStream(QSGMaterialShader::RenderState &state) noexcept
+        : bytes(state.uniformData()->data())
+    {
+    }
+
+    ~UniformDataStream()
+    {
+    }
+
+    template<typename Data>
+    friend inline UniformDataStream &operator<<(UniformDataStream &stream, const Data &data)
+    {
+        stream.applyPadding<Data>();
+        memcpy(stream.bytes, &data, sizeof(Data));
+        stream.bytes += sizeof(Data);
+        stream.updatePadding<Data>();
+        return stream;
+    }
+
+    template<typename Data>
+    inline void skip(const Data &data = {})
+    {
+        applyPadding<Data>();
+        Q_UNUSED(data);
+        bytes += sizeof(Data);
+        updatePadding<Data>();
+    }
+
+    inline void skipComponents(uint count)
+    {
+        if (padding < 16) {
+            bytes += padding;
+            padding = 16;
+        }
+        bytes += (4 * count);
+    }
+
+    friend inline UniformDataStream &operator<<(UniformDataStream &stream, const QMatrix4x4 &m)
+    {
+        stream.applyPadding<QMatrix4x4>();
+        memcpy(stream.bytes, m.constData(), 4 * 4 * 4);
+        stream.bytes += 4 * 4 * 4;
+        return stream;
+    }
+
+    template<typename T>
+    friend inline UniformDataStream &operator<<(UniformDataStream &stream, const QVector<T> &v)
+    {
+        for (const auto &item : v) {
+            stream.applyPadding<T>();
+            memcpy(stream.bytes, &item, sizeof(T));
+            stream.bytes += sizeof(T);
+        }
+        return stream;
+    }
+
+    char *bytes;
+    size_t padding = 16;
+
+private:
+    template<typename Data>
+    inline void applyPadding()
+    {
+        if (padding < sizeof(Data) && padding < 16) {
+            bytes += padding;
+            padding = 16;
+        }
+    }
+    template<typename Data>
+    inline void updatePadding()
+    {
+        if constexpr (sizeof(Data) % 16 != 0)
+            padding -= sizeof(Data) % 16;
+    }
+};
+
 class SDFShader : public QSGMaterialShader
 {
 public:
